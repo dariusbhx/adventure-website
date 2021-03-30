4dfd3a27d3e9f8bb99ca20346ac913d549eae553
darius <dariusbrown1999@hotmail.com>
2021-03-30 13:10:21 +0100

    initial commit

---
+++ website/scripts.js
@@ -0,0 +1,13 @@
+const selectElement = (s) => document.querySelector(s)
+
+//Open the menu on click
+
+selectElement(s ='.open').addEventListener('click', () => {
+    selectElement(s = '.nav-list').classList.add('active') 
+})
+
+//Close the menu on click
+selectElement(s ='.close').addEventListener('click', () => {
+    selectElement(s = '.nav-list').classList.remove('active')
+})
+
